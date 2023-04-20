438d1c7cd6264e58243d0114d9aa9586ce7741b4
LucianoAlmeida93 <117646493+LucianoAlmeida93@users.noreply.github.com>
2023-04-19 21:35:45 -0300

    Add files via upload
    #include <stdio.h>
    
    int main()
    {
        int l_69, valor;
       
        
        if(&l_69>&valor){
            printf("\nO endereço de p_1: (%p), e maior que o endereço de valor: (%p)\n\n", &l_69, &valor);
        }else{
            printf("\nO endereço de valor: (%p) é maior que o endereço de p_1: (%p)\n\n", &valor, &l_69);
    
        
        return 0;
        }
    }

---
+++ main (7).c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int l_69, valor;
+   
+    
+    if(&l_69>&valor){
+        printf("\nO endereço de p_1: (%p), e maior que o endereço de valor: (%p)\n\n", &l_69, &valor);
+    }else{
+        printf("\nO endereço de valor: (%p) é maior que o endereço de p_1: (%p)\n\n", &valor, &l_69);
+
+    
+    return 0;
+    }
+}
